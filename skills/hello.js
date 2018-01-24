690690d7ec30612fb13d7cf13153f94182dbd3f7
nirmala <nirmala.jraman@gmail.com>
2018-01-24 11:30:40 -0800

    Hear: Ask Question

--- skills/hello.js
+++ skills/hello.js
@@ -7,7 +7,14 @@ This module handles the hello message.
 module.exports = function(controller) {
 
   controller.hears(['^hello$'], 'direct_message,direct_mention', function(bot, message) {
-        bot.reply(message, "Hi there, how are you feeling today?");
+        // bot.reply(message, "Hi there, how are you feeling today?");
+
+        convo.addQuestion('Hi friend! How are you feeling today?',function(response,convo) {
+
+      convo.say('You said: ' + response.text);
+      convo.next();
+
+    },{},'default');
     });
 
 };

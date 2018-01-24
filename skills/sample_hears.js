d54eeffffa21dc71a69a7bb2dcabf6232439839b
nirmala <nirmala.jraman@gmail.com>
2018-01-24 11:08:59 -0800

    Hear: hello enhanced

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -28,7 +28,8 @@ module.exports = function(controller) {
     });
 
 	controller.hears(['^hello$'], 'direct_message,direct_mention', function(bot, message) {
-        bot.reply(message, "Hi there, you're on workspace: " + message.team + " " + message.user)
+bot.reply(message, "Oh Hello");
+        bot.reply(message, "Hi there, you're on workspace: " + message.team);
     });
 
 

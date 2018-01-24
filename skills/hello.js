63041d02cb11a2c5ae05941f4db3bb6acd8d738a
nirmala <nirmala.jraman@gmail.com>
2018-01-24 11:38:22 -0800

    Ask Question: Fix

--- skills/hello.js
+++ skills/hello.js
@@ -1,20 +1,21 @@
-/*
+  /*
 
-This module handles the hello message.
+  This module handles the hello message.
 
-*/
+  */
 
-module.exports = function(controller) {
+  module.exports = function(controller) {
 
-  controller.hears(['^hello$'], 'direct_message,direct_mention', function(bot, message) {
-        // bot.reply(message, "Hi there, how are you feeling today?");
+    controller.hears(['^hello$'], 'direct_message,direct_mention', function(bot, message) {
+          // bot.reply(message, "Hi there, how are you feeling today?");
+          bot.startConversation(message, function(err, convo) {
+            convo.addQuestion('Hi friend! How are you feeling today?',function(response,convo) {
 
-        convo.addQuestion('Hi friend! How are you feeling today?',function(response,convo) {
+              convo.say('You said: ' + response.text);
+              convo.next();
 
-      convo.say('You said: ' + response.text);
-      convo.next();
-
-    },{},'default');
+            },{},'default');
+          });
     });
 
-};
+  };

5e4013504561af3714aa1361ed7530f668f26307
nirmala <nirmala.jraman@gmail.com>
2018-01-24 15:27:04 -0800

    Trial # 1

--- skills/feeling.js
+++ skills/feeling.js
@@ -16,6 +16,7 @@ module.exports = function(controller) {
         bot.startConversation(message, function(err, convo) {
             convo.ask('What is your feeling right now?', function(response, convo) {
 
+                console.log("Response for feeling = " + response);
                 convo.say('You said that you are feeling ' + response.text);
                 convo.next();
 
--- skills/hello.js
+++ skills/hello.js
@@ -7,8 +7,7 @@
   module.exports = function(controller) {
 
     controller.hears(['^hello$'], 'direct_message,direct_mention', function(bot, message) {
-          // bot.reply(message, "Hi there, how are you feeling today?");
-          console.log(message);
+          console.log("message = " + message);
           bot.startConversation(message, function(err, convo) {
             convo.addQuestion('Hi friend! What is your name?',function(response_name,convo) {
 

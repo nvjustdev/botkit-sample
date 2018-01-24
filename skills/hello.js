882c2dd5e4243158cf725941df994bc1e28b53e9
nirmala <nirmala.jraman@gmail.com>
2018-01-24 11:49:27 -0800

    Updated user

--- skills/hello.js
+++ skills/hello.js
@@ -9,7 +9,7 @@
     controller.hears(['^hello$'], 'direct_message,direct_mention', function(bot, message) {
           // bot.reply(message, "Hi there, how are you feeling today?");
           bot.startConversation(message, function(err, convo) {
-            convo.addQuestion('Hi  <@${user}> ! How are you feeling today?',function(response,convo) {
+            convo.addQuestion('Hi  @${user} ! How are you feeling today?',function(response,convo) {
 
               convo.say('You said: ' + response.text);
               convo.next();

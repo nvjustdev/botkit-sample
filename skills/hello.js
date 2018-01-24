a4214d6a142144efd16280bcc18c97a037048ba3
nirmala <nirmala.jraman@gmail.com>
2018-01-24 11:52:12 -0800

    Trying to get user name

--- skills/hello.js
+++ skills/hello.js
@@ -8,8 +8,9 @@
 
     controller.hears(['^hello$'], 'direct_message,direct_mention', function(bot, message) {
           // bot.reply(message, "Hi there, how are you feeling today?");
+          console.log(message);
           bot.startConversation(message, function(err, convo) {
-            convo.addQuestion('Hi  @${user} ! How are you feeling today?',function(response,convo) {
+            convo.addQuestion('Hi  @'+${message.user} +'! How are you feeling today?',function(response,convo) {
 
               convo.say('You said: ' + response.text);
               convo.next();

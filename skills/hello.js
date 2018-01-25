d6e90be0e213c568453a539389e710e8b2a18011
nirmala <nirmala.jraman@gmail.com>
2018-01-25 11:53:29 -0800

    Adding feelings

--- skills/hello.js
+++ skills/hello.js
@@ -12,20 +12,69 @@
             convo.addQuestion('Hi friend! What is your name?',function(response_name,convo) {
 
               // convo.say('Hi ' + response_name.text + '! How are you feeling today?');
-              // convo.next();
+              convo.next();
 
               console.log("name " + response_name.text);
 
-              // bot.startConversation(response_name, function(err, convo) {
+              bot.startConversation(response_name, function(err, convo) {
                   convo.say('Hi ' + response_name.text + '!');
 
-                  convo.ask('How are you feeling today?', function(feeling_response, convo) {
+                  bot.createConversation(message, function(err, convo) {
 
-                      convo.say('You said ' + feeling_response.text);
-                      convo.next();
+                      // create a path for when a user says YES
+                      convo.addMessage({
+                              text: 'Oh no! What made you rage?!',
+                      },'yes_thread');
 
-                  });
-              // });
+                      // create a path for when a user says NO
+                      // mark the conversation as unsuccessful at the end
+                      convo.addMessage({
+                          text: 'Oh no! Didn\'t know that.',
+                          action: 'stop', // this marks the converation as unsuccessful
+                      },'sad_thread');
+
+                      // create a path where neither option was matched
+                      // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
+                      convo.addMessage({
+                          text: 'Sorry I did not understand. Just tell me your feeling',
+                          action: 'default',
+                      },'bad_response');
+
+                      // Create a yes/no question in the default thread...
+                      convo.ask('Do you like cheese?', [
+                          {
+                              pattern:  ['^angry$'],
+                              callback: function(response, convo) {
+                                  convo.gotoThread('angry_thread');
+                              },
+                          },
+                          {
+                              pattern:  ['^sad$'],
+                              callback: function(response, convo) {
+                                  convo.gotoThread('sad_thread');
+                              },
+                          },
+                          {
+                              default: true,
+                              callback: function(response, convo) {
+                                  convo.gotoThread('bad_response');
+                              },
+                          }
+                      ]);
+
+                      convo.activate();
+
+                      // capture the results of the conversation and see what happened...
+                      convo.on('end', function(convo) {
+
+                          if (convo.successful()) {
+                              // this still works to send individual replies...
+                              bot.reply(message, 'Wish you a nice day!');
+                          }
+
+                      });
+                  }
+              });
 
             },{},'default');
           });

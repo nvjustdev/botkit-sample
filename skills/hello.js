16a78427b52f7a78a259ef605523d052642473f7
nirmala <nirmala.jraman@gmail.com>
2018-01-25 11:41:00 -0800

    hello JS update

--- skills/hello.js
+++ skills/hello.js
@@ -16,7 +16,7 @@
 
               console.log("name " + response_name.text);
 
-              bot.startConversation(message, function(err, convo) {
+              bot.startConversation(response_name, function(err, convo) {
                   convo.say('Hi ' + response_name.text + '!');
 
                   convo.ask('How are you feeling today?', function(feeling_response, convo) {

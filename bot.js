9d5e111aeb5e85872f752304b1b7b5e88e2ddb33
nirmala <nirmala.jraman@gmail.com>
2018-01-27 16:28:58 -0800

    using watson

--- bot.js
+++ bot.js
@@ -164,22 +164,31 @@ if (!process.env.clientId || !process.env.clientSecret) {
   // controller.studio.before, controller.studio.after and controller.studio.validate
   if (process.env.studio_token) {
       controller.on('direct_message,direct_mention,mention', function(bot, message) {
-          controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
-              if (!convo) {
-                  // no trigger was matched
-                  // If you want your bot to respond to every message,
-                  // define a 'fallback' script in Botkit Studio
-                  // and uncomment the line below.
-                  // controller.studio.run(bot, 'fallback', message.user, message.channel);
-              } else {
-                  // set variables here that are needed for EVERY script
-                  // use controller.studio.before('script') to set variables specific to a script
-                  convo.setVar('current_time', new Date());
-              }
-          }).catch(function(err) {
-              bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-              debug('Botkit Studio: ', err);
-          });
+        middleware.interpret(bot, message, function (err) {
+          console.console.log("middleware to interpret");
+            if (!err) {
+                sharedCode.handleWatsonResponse(bot, message, 'slack');
+            }
+            else {
+                bot.reply(message, "I'm sorry, but for technical reasons I can't respond to your message");
+            }
+        });
+          // controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
+          //     if (!convo) {
+          //         // no trigger was matched
+          //         // If you want your bot to respond to every message,
+          //         // define a 'fallback' script in Botkit Studio
+          //         // and uncomment the line below.
+          //         // controller.studio.run(bot, 'fallback', message.user, message.channel);
+          //     } else {
+          //         // set variables here that are needed for EVERY script
+          //         // use controller.studio.before('script') to set variables specific to a script
+          //         convo.setVar('current_time', new Date());
+          //     }
+          // }).catch(function(err) {
+          //     bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
+          //     debug('Botkit Studio: ', err);
+          // });
       });
   } else {
       console.log('~~~~~~~~~~');
